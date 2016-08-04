69c792cbc51fd4c0495698f389b7334bb3314b94
tcsalameh <tomsalameh@gmail.com>
2016-08-04 21:34:37 +0000

    Adds support for StartTLS on chain overlay
    This commit allows the olcDbStartTLS attribute to be set on the
    chain overlay, as chain_tls. This lets the user specify how the
    replica LDAP servers should use the StartTLS operation when
    communicating with the primary.

--- spec/classes/openldap_server_spec.rb
+++ spec/classes/openldap_server_spec.rb
@@ -1062,6 +1062,7 @@ describe 'openldap::server' do
                 :chain_id_assert_bind => 'mode=self flags=proxy-authz-critical',
                 :chain_rebind_as_user => true,
                 :chain_return_error   => true,
+                :chain_tls            => 'start',
                 :syncrepl             => [
                   'rid=001 provider=ldap://ldap.example.com/',
                 ],
@@ -1094,6 +1095,7 @@ describe 'openldap::server' do
               'olcDbURI'          => ['ldap://ldap.example.com/'],
               'olcDbRebindAsUser' => ['TRUE'],
               'olcDbIDAssertBind' => ['mode=self flags=proxy-authz-critical'],
+              'olcDbStartTLS'     => ['start'],
             }
           ) }
           it { should contain_openldap('olcDatabase={2}hdb,cn=config').with_attributes(
